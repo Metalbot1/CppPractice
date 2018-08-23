bc4bae2a33a88dbe5af05102f791dc4a0ca26362
cvanesse <cvanesse@ualberta.ca>
2018-08-23 14:25:06 -0600

    Implementation of task and job holder variables, as well as functions to interact with them.

--- parallelism/thread_pool.cpp
+++ parallelism/thread_pool.cpp
@@ -38,3 +38,39 @@ void ThreadPool::worker_main()
         //Here's where our worker code goes.
     }
 }
+
+//Not sure that I fully understand this function.
+int ThreadPool::task_create(){
+
+    std::lock_guard<std::mutex> lock(_mutex);
+    int task;
+
+    if(_free_tasks.empty()){ // if there are no "free" tasks?
+        task = static_cast<int>(_task_size); // store our task_size in return variable (why?)
+        _task_size++; // increase task_size (makes sense)
+        _task_pending_count.resize(_task_size); // resizes the task_pending queue
+    }else{
+        task = _free_tasks.back(); // take the back element of free_tasks as our return variable (why?)
+        _free_tasks.pop_back(); // remove final element of "free_tasks" (ie the one we are going to return.)
+    }
+    _task_pending_count[task] = 0u; // set the this task id to pending?
+
+    return task; // really dont know what we're trying to return.
+}
+
+//Checks if the task identified by the input is completed. Simple.
+bool ThreadPool::task_finished(int task) const{
+    std::lock_guard<std::mutex> lock(_mutex);
+
+    return _task_pending_count[task] == 0;
+}
+
+//Adds a job to a task.
+void ThreadPool::task_perform(int task, const Job& job){
+    std::lock_guard<std::mutex> lock(_mutex);
+
+    _job_size++; // Not sure what job_size is for yet
+    _job_task.push_back(task); //Record which task this job belongs to in our job_test vector.
+    _job_function.push_back(job); //Record what function to run to complete this job.
+    _task_pending_count[task]++; //Increment the record of how many jobs remain for this task
+}
--- parallelism/thread_pool.h
+++ parallelism/thread_pool.h
@@ -24,4 +24,12 @@ private:
     bool _running;
     mutable std::mutex _mutex;
     std::vector<std::thread> _threads;
+
+    size_t _task_size;
+    std::vector<int> _free_tasks; // "free" list for tasks. For reusing memory
+    std::vector<unsigned int> _task_pending_count; // per-task count of how many jobs are pending
+
+    size_t _job_size;
+    std::vector<int> _job_task; // maps every job to the task it belongs to
+    std::vector<Job> _job_function; // stores the functions linked to each job
 };
